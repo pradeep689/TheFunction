db4a8c97e51ffb6ec090c62e5a4f1c8f035f4428
pradeep689 <65967180+pradeep689@users.noreply.github.com>
2020-05-27 08:24:01 +0530

    Update Function.c

--- Function.c
+++ Function.c
@@ -1,9 +1,10 @@
-def greet(name):
+def greet(sankar):
     """
     This function greets to
     the person passed in as
     a parameter
     """
-    print("vanakam " + name + ". Good Night");
-    greet("Sankar")
-
+{
+    print("vanakam " + Sankar + ". Good Night");
+    greet("Sankar");
+}
